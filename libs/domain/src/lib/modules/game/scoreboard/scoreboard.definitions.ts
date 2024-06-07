465c774f02647aeab2c865fd3f82abf350f07a87
Maxime Parriaux <maxime@dotfile.com>
2024-06-08 01:59:01 +0200

    feat: 🔢 compute score

--- libs/domain/src/lib/modules/game/scoreboard/scoreboard.definitions.ts
+++ libs/domain/src/lib/modules/game/scoreboard/scoreboard.definitions.ts
@@ -4,13 +4,14 @@ import { DiceModel } from '../../../model';
 const sumDice = (dices: DiceModel[]) =>
   dices.reduce((acc, dice) => acc + (dice.currentFace?.value ?? 0), 0);
 
+// Better unit test that some day
 const findNOfAKind = (n: number, dices: DiceModel[]) => {
   const dicesPerFaceValue = dices.reduce<Record<number, number>>(
     (acc, dice) => {
       if (dice.currentFace)
         return {
           ...acc,
-          [dice.currentFace.value]: acc[dice.currentFace.value] ?? 0 + 1,
+          [dice.currentFace.value]: (acc[dice.currentFace.value] ?? 0) + 1,
         };
       return acc;
     },
@@ -22,6 +23,29 @@ const findNOfAKind = (n: number, dices: DiceModel[]) => {
   )?.[0];
 
   return Number(value ?? 0);
+};
+
+// Better unit test that some day
+const findNStraight = (n: number, dices: DiceModel[]): boolean => {
+  const values = dices.map((dice) => dice.currentFace?.value ?? 0);
+  const sortedValues = [...new Set([...values])].sort((a, b) =>
+    a > b ? 1 : -1
+  );
+
+  let maxCount = 0;
+  let count = 1;
+  for (let i = 0; i < sortedValues.length - 1; i++) {
+    if (sortedValues[i] + 1 === sortedValues[i + 1]) {
+      count++;
+      if (count > maxCount) {
+        maxCount = count;
+      }
+    } else {
+      count = 0;
+    }
+  }
+
+  return maxCount >= n;
 };
 
 export const scoreboardDefinitions: Record<
@@ -75,19 +99,39 @@ export const scoreboardDefinitions: Record<
   },
   full: {
     name: 'Full',
-    computeValue: () => 25,
+    computeValue: (dices) => {
+      const threeOfAKind = findNOfAKind(3, dices);
+      const otherDices = dices.filter(
+        (dice) => dice.currentFace?.value !== threeOfAKind
+      );
+
+      const [dice1, dice2] = otherDices;
+
+      if (
+        otherDices.length === 2 &&
+        dice1.currentFace?.value === dice2.currentFace?.value
+      ) {
+        return 25;
+      }
+
+      return 0;
+    },
   },
   small_straight: {
     name: 'Small straight',
-    computeValue: () => 30,
+    computeValue: (dices) => {
+      return findNStraight(4, dices) ? 30 : 0;
+    },
   },
   straight: {
     name: 'Straight',
-    computeValue: () => 40,
+    computeValue: (dices) => {
+      return findNStraight(5, dices) ? 40 : 0;
+    },
   },
   five_of_a_kind: {
     name: 'Yams',
-    computeValue: () => 50,
+    computeValue: (dices) => (findNOfAKind(5, dices) ? 50 : 0),
   },
   lucky: {
     name: '☘',
--- libs/frontend/components/src/lib/modules/game/scoreboard/scoreboard.tsx
+++ libs/frontend/components/src/lib/modules/game/scoreboard/scoreboard.tsx
@@ -1,26 +1,41 @@
 import { useTranslation } from 'react-i18next';
 import styles from './scoreboard.scss';
 import { useGameContext } from '@yams-tactics/frontend-common';
+import { scoreboardDefinitions } from '@yams-tactics/domain';
 
 interface ScoreBoardProps {
   className?: string;
 }
 
+// @TODO - Add a context for those dice animation to be able to display loading state on precompute
 export function ScoreBoard({ className = '' }: ScoreBoardProps) {
   const { t } = useTranslation();
-  const { currentPlayer } = useGameContext();
-  const scoreboard = currentPlayer?.scoreboard;
+  const { currentPlayer, game } = useGameContext();
+  const scores = currentPlayer?.scoreboard.scores.map((score) => {
+    return {
+      ...score,
+      value: scoreboardDefinitions[score.type].computeValue(
+        currentPlayer.dices
+      ),
+    };
+  });
 
   return (
     <div className={`${className} ${styles.scoreboard} shadow`}>
       <table>
         <tbody>
-          {(scoreboard?.scores ?? []).map((score) => (
+          {(scores ?? []).map((score) => (
             <tr key={score.id}>
               <td className={styles.rawName}>
                 {t(`scoreboard.${score.type}`)}
               </td>
-              <td>{score.done ? score.value || 0 : ''}</td>
+              <td className={styles.possible}>
+                {game?.currentRound.startsWith('shop')
+                  ? score.done
+                    ? score.value
+                    : ''
+                  : score.value}
+              </td>
             </tr>
           ))}
         </tbody>
